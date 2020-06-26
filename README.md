96d8c2683675bab911c85ebcf52a4d07a3c0b016
aitsamahad <aitsama.ahad@gmail.com>
2020-06-26 12:28:47 +0500

    NextJS Blog with Portfolio.

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# Strapi application
-
-A quick description of your strapi application
+This is a starter template for [Learn Next.js](https://nextjs.org/learn).
---
+++ pages/portfolio/[id].js
@@ -0,0 +1,40 @@
+import Layout from "../../components/layout";
+import Head from "next/head";
+import { getAllPortfolioIds, getPortfolioData } from "../../lib/portfolio";
+import Date from "../../components/date";
+
+import utilStyles from "../../styles/utils.module.css";
+
+export default function Post({ portfolioData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{portfolioData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{portfolioData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={portfolioData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: portfolioData.contentHtml }} />
+      </article>
+    </Layout>
+  );
+}
+
+export async function getStaticPaths() {
+  const paths = getAllPortfolioIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const portfolioData = await getPortfolioData(params.id);
+  return {
+    props: {
+      portfolioData,
+    },
+  };
+}
