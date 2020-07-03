825abcd41a43a0b8717202db69adf9b324c915d9
aitsamahad <aitsama.ahad@gmail.com>
2020-07-03 13:24:40 +0500

    Image Optimization Added!

--- next.config.js
+++ next.config.js
@@ -1,3 +1,17 @@
+const withPlugins = require("next-compose-plugins");
+const optimizedImages = require("next-optimized-images");
+
+module.exports = withPlugins([
+  [
+    optimizedImages,
+    {
+      /* config for next-optimized-images */
+    },
+  ],
+
+  // your other plugins here
+]);
+
 module.exports = {
   webpack: config => {
     config.plugins = config.plugins || [];
